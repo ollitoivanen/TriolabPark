be5be7b8e1e706e1c0a20a39963dc1545751b096
Olli Toivanen <olli.j.toivanen@gmail.com>
2018-12-23 13:23:05 +0200

    android firebase working

--- App.js
+++ App.js
@@ -6,24 +6,88 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity
+} from "react-native";
+import ListItem from "./src/ListItem";
+import firebase from "react-native-firebase";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: [],
+      subscribed: false
+    };
+    this.unsubscribe = null;
+    this.ref = firebase.firestore().collection("People");
+    // this.getPeople();
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  componentDidMount = () => {
+    this.setState({ subscribed: true });
+
+    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+  };
+  componentWillUnmount() {
+    if (this.state.subscribed === true) {
+      this.unsubscribe();
+    }
+  }
+  onCollectionUpdate = querySnapshot => {
+    const people = [];
+    querySnapshot.forEach(doc => {
+      people.push({
+        key: doc.id,
+
+        id: doc.id,
+        state: doc.data().state,
+        name: doc.data().name
+      });
+    });
+    this.setState({ people });
+  };
+
+  getPeople = () => {
+    const people = [];
+    firebase
+      .firestore()
+      .collection("People")
+      .get()
+      .then(doc => {
+        doc.forEach(doc => {});
+      });
+  };
+
+  toggleSpot = (state, id) => {
+    firebase
+      .firestore()
+      .collection("People")
+      .doc(id)
+      .update({
+        state: !state
+      });
+  };
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <FlatList
+          data={this.state.people}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.item}
+              onPress={() => this.toggleSpot(item.state, item.id)}
+            >
+              <ListItem {...item} />
+            </TouchableOpacity>
+          )}
+        />
       </View>
     );
   }
@@ -32,18 +96,20 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
   },
+  item: {
+    width: "100%"
+  }
 });
